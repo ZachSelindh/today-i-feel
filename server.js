36682df8408e1c59770de90f474cfd14c2493b0a
Zach Selindh <zachselindh@gmail.com>
2019-10-01 19:16:03 -0500

    Added directories and server associations.

--- server.js
+++ server.js
@@ -15,9 +15,24 @@ app.use(
 // Setup enviroment for static path
 if (process.env.ENVIRONMENT === "development") {
   // Dev route
-  console.log("Development env");
+  console.log("Server is operating in a development enviroment");
   app.use(express.static(path.join(__dirname, "client")));
 } else if (process.env.ENVIRONMENT === "production") {
   // Build route
   app.use(express.static(path.join(__dirname, "client", "build")));
 }
+
+const routes = require("./routes");
+app.use(routes);
+
+const connection = require("./config/mongo-connection");
+connection
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 3001;
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
